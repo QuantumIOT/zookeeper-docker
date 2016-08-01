bf67670a93a91e23fbc049fb90803a7dc6964bcd
gprivitera <gprivitera@streamatica.com>
2016-08-01 15:39:55 -0500

    [#1] Switch to alpine as base image

--- zk-init.sh
+++ zk-init.sh
@@ -4,19 +4,18 @@ ZK=$1
 IPADDRESS=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
 MYID=$(echo $IPADDRESS | sed 's/\.//g')
 
-cd /tmp/zookeeper
+cd /opt/zookeeper
 if [ -n "$ZK" ]
 then
-  ./bin/zkCli.sh -server $ZK:2181 get /zookeeper/config | grep ^server >> /tmp/zookeeper/conf/zoo.cfg
-  echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> /tmp/zookeeper/conf/zoo.cfg
-  cp /tmp/zookeeper/conf/zoo.cfg /tmp/zookeeper/conf/zoo.cfg.org
-  /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start
-  sleep 5
-  /tmp/zookeeper/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$IPADDRESS:2888:3888:participant;2181"
-  while true; do sleep 10000; done # block forever
+  ./bin/zkCli.sh -server $ZK:2181 get /zookeeper/config | grep ^server >> /opt/zookeeper/conf/zoo.cfg
+  echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> /opt/zookeeper/conf/zoo.cfg
+  cp /opt/zookeeper/conf/zoo.cfg /opt/zookeeper/conf/zoo.cfg.org
+  /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
+  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /opt/zookeeper/bin/zkServer.sh start-foreground
+  sleep 1
+  /opt/zookeeper/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$IPADDRESS:2888:3888:participant;2181"
 else
-  echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> /tmp/zookeeper/conf/zoo.cfg
-  /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
+  echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> /opt/zookeeper/conf/zoo.cfg
+  /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
+  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /opt/zookeeper/bin/zkServer.sh start-foreground
 fi
