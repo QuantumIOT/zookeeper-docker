6af30f960949bdbfbe521ce0348403c213ca2575
gprivitera <gprivitera@streamatica.com>
2016-11-22 10:34:50 -0600

    Refactor init script and Dockerfile installation

--- zk-init.sh
+++ zk-init.sh
@@ -1,21 +1,35 @@
-#!/bin/bash
+#!/bin/sh
 
-ZK=$1
-IPADDRESS=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
-MYID=$(echo $IPADDRESS | sed 's/\.//g')
+set -e
 
-cd /opt/zookeeper
-if [ -n "$ZK" ]
-then
-  /opt/zookeeper/bin/zkCli.sh -server $ZK:2181 get /zookeeper/config | grep ^server >> /opt/zookeeper/conf/zoo.cfg
-  echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> /opt/zookeeper/conf/zoo.cfg
-  cp /opt/zookeeper/conf/zoo.cfg /opt/zookeeper/conf/zoo.cfg.org
-  /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /opt/zookeeper/bin/zkServer.sh start-foreground
-  sleep 10
-  /opt/zookeeper/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$IPADDRESS:2888:3888:participant;2181"
+if [ -f "/etc/zookeeper/conf/zoo.cfg" ]; then
+    echo "Zookeeper configuration file found in /etc/zookeeper/conf/zoo.cfg"
 else
-  echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> /opt/zookeeper/conf/zoo.cfg
-  /opt/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /opt/zookeeper/bin/zkServer.sh start-foreground
+    if [ -z "${ZK_CONFIG}" ]; then
+        echo "Zookeeper config not found, here are the options:"
+        echo "  - bind-mount /etc/zookeeper/conf/zoo.cfg"
+        echo "  - specify ZK_CONFIG env variable with config values"
+        exit 1
+    fi
+
+    echo "dataDir=/var/lib/zookeeper" > "/etc/zookeeper/conf/zoo.cfg"
+
+    for line in $(echo "${ZK_CONFIG}" | tr "," "\n"); do
+        echo "$line" >> "/etc/zookeeper/conf/zoo.cfg"
+    done
 fi
+
+if [ -e "/var/lib/zookeeper/myid" ]; then
+    echo "Zookeeper node id found in /var/lib/zookeeper/myid"
+else
+    if [ -z "${ZK_ID}" ]; then
+        echo "zookeeper node id not found, here are your options:"
+        echo "  - bind-mount /var/lib/zookeeper with myid"
+        echo "  - specify ZK_ID env variable with id"
+        exit 1
+    fi
+
+    echo "${ZK_ID}" > "/var/lib/zookeeper/myid"
+fi
+
+exec /usr/share/zookeeper/bin/zkServer.sh start-foreground
